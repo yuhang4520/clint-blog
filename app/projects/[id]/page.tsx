6a1e5e796937e5d57c78e2c1b0deb04eb62d0b2f
yuhang4520 <yuh4520@163.com>
2025-07-02 11:14:11 +0800

    update: 文章更新

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { genPageMetadata } from 'app/seo'
 import projectsData from '@/data/projectsData'
-import Image from 'next/image'
+import Image from '@/components/Image'
 import { Project } from '@/data/projectsData'
-export const metadata = genPageMetadata({ title: 'XX公路智慧监测预警' })
+export const metadata = genPageMetadata({ title: 'clint`s projects' })
 
 export default function Projects({ params }) {
   const data: Project | undefined = projectsData.find((el) => params?.id === el?.id)
@@ -12,7 +12,7 @@ export default function Projects({ params }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            作品
+            {data?.title}
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{data?.content}</p>
         </div>
